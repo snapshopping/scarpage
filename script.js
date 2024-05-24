a920af08e36cdcde5dc54accb8ebb76507a48e8c
snapshopping <168413173+snapshopping@users.noreply.github.com>
2024-05-24 20:58:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-function toggleVisibility() {
-    var additionalContent = document.getElementById("additionalContent");
-    var hideButton = document.getElementById("hideButton");
-    if (additionalContent.style.display === "none") {
-        additionalContent.style.display = "block";
-        hideButton.style.display = "inline-block";
-    } else {
-        additionalContent.style.display = "none";
-        hideButton.style.display = "none";
-    }
-}
-
-
 
   
